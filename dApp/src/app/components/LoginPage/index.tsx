775f26ec5e674778a0980c08e9e9db6d01b83dc7
Jingyu <jingyu.zhang@appfolio.com>
2018-06-09 11:50:09 -0700

    oauth with github using gatekeeper todo: clean up code, use a different redirect URI

--- dApp/src/app/components/LoginPage/index.tsx
+++ dApp/src/app/components/LoginPage/index.tsx
@@ -1,17 +1,56 @@
 import * as React from 'react';
 import { Button } from 'reactstrap';
 
-export class LoginPage extends React.Component {
+const CLIENT_ID = 'd345adcdaf9142e9cc19';
+const REDIRECT_URI = 'http://localhost:3000/';
+
+export namespace LoginPage {
+  export interface State {
+    token?: string,
+    isLogined: boolean,
+  }
+}
+
+export class LoginPage extends React.Component<{}, LoginPage.State> {
+  constructor(props?: any, context?: any) {
+    super(props, context);
+
+    this.state = {
+      token: '',
+      isLogined: false,
+    };
+  }
+
+  componentDidMount() {
+    const oauth = window.location.href.match(/\?code=(.*)/);
+    if (oauth) {
+      const code = oauth.length === 2 && oauth[1] || null;
+      fetch(`https://githubcolonygatekeeper.herokuapp.com/authenticate/${code}`)
+        .then(response => response.json())
+        .then(({ token }) => {
+          this.setState({
+            token,
+            isLogined: true,
+          });
+        });
+    }
+  }
+
   onClickLoginBtn = () => {
-    console.log('yay');
+    window.location.href = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=user&redirect_uri=${REDIRECT_URI}`;
   }
 
   render() {
     return (
-      <Button onClick={this.onClickLoginBtn} className="btn-social btn-github" style={{ height: '35px' }}>
-        <i className="fab fa-github" />
-        Login With Github
-      </Button>
-    )
+      <div>
+        {this.state.isLogined && (<h1> Welcome</h1>)}
+        {!!!this.state.isLogined && (
+          <Button onClick={this.onClickLoginBtn} className="btn-social btn-github" style={{ height: '35px' }}>
+            <i className="fab fa-github" />
+            Login With Github
+          </Button>
+        )}
+      </div>
+    );
   }
 }
