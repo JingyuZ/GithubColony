c5d0688887ed46437fa8d87e303f6775b6ba51fd
jordanellis6809 <jordanellis6809@gmail.com>
2018-06-13 20:10:17 -0700

    Adding & building dapp code + serving it statically works

--- tools/ipfs-auto-deploy/index.js
+++ tools/ipfs-auto-deploy/index.js
@@ -1,12 +1,14 @@
 // TODO:
-// - build content
-// - startup IPFS and deploy the content (maintain a node with the content)
+// - startup IPFS and deploy the content (maintain a node with the content) (content at ./app/dist/ folder)
 // - get the rootURL
 // - forward all requests to the IPFS URL
 
+const path = require('path');
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => res.send('We\'re here! ipfs-auto-deploy says "hello"!'));
+
+app.use('/static', express.static(path.join(__dirname, 'app/dist')));
 
 app.listen(8888, '0.0.0.0', () => console.log('ipfs-auto-deploy listening on port 8888'));
